1119f3f51765c382e7f0a55453325fdb2e0baf88
gikolgi-dev <gikolgi@gmail.com>
2026-01-14 22:19:02 +0100

    rounded press animation for cards
    Signed-off-by: gikolgi-dev <gikolgi@gmail.com>

--- app/src/main/java/com/example/attempt3/ui/HabitItemCard.kt
+++ app/src/main/java/com/example/attempt3/ui/HabitItemCard.kt
@@ -159,6 +159,7 @@ fun HabitItemCard(
         modifier = modifier
             .fillMaxWidth()
             .padding(horizontal = 12.dp, vertical = 0.dp)
+            .clip(RoundedCornerShape(8.dp))
             .clickable(onClick = onClick),
         shape = RoundedCornerShape(8.dp),
         colors = CardDefaults.cardColors(
