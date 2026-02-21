69110471804e221b1275834dccb69dc5c41b797d
gikolgi-dev <gikolgi@gmail.com>
2026-02-21 13:13:40 +0100

    Ver number Signed-off-by: gikolgi-dev <gikolgi@gmail.com>
    Signed-off-by: gikolgi-dev <gikolgi@gmail.com>

--- app/src/main/java/com/example/attempt3/ui/screen/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/attempt3/ui/screen/settings/SettingsScreen.kt
@@ -532,7 +532,7 @@ fun SettingsScreen(onDismiss: () -> Unit, db: HabitDatabase, settingsDataStore: 
                             )
                             Spacer(modifier = Modifier.height(2.dp))
                             Text(
-                                text = "Version 2.0.3",
+                                text = "Version 2.0.4",
                                 style = MaterialTheme.typography.bodySmall,
                                 color = Color.Gray
                             )
